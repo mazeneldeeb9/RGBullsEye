b4cf8b3bf1af1c44d100c73d11499d48e367bb40
mazeneldeeb9 <mazeneldeeb9@gmail.com>
2024-08-12 11:24:27 +0300

    Initial Commit

---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by mazen eldeeb on 12/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
